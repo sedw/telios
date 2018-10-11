d27c1593ad86d8114839e76b77cc0cb910af91ce
unknown <sedwlc@gmail.com>
2018-10-12 01:06:15 +0900

    Change filename

--- telios.py
+++ telios.py
@@ -1,27 +1,25 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-# Telnet and run commands 
-
 import sys
 import os
 import telnetlib
 
 
 ################################################################################
-# Telnet client setting
+# telnet クライアントの設定
 ################################################################################
 
 TCPPORT = 23
 TIMEOUT = 5
 
 host = sys.argv[1]
-user = os.environ['TARC_USER']
-password = os.environ['TARC_PASSWORD']
+user = os.environ['TELIOS_USERNAME']
+password = os.environ['TELIOS_PASSWORD']
 
 
 ################################################################################
-# Cisco IOS commands
+# Cisco IOS 用の設定
 ################################################################################
 
 USER_PROMPT = b"Username:"
@@ -34,32 +32,31 @@ commands = sys.stdin
 
 
 ################################################################################
-# Telnet and run commands
+# telnet 接続とコマンド入力
 ################################################################################
 
-# Start Telnet
+# telnet 開始
 tn = telnetlib.Telnet(host, TCPPORT, TIMEOUT)
 
-# Login
+# ログイン処理
 tn.read_until(USER_PROMPT)
 tn.write(user.encode('ascii') + b"\n")
 tn.read_until(PASSWORD_PROMPT)
 tn.write(password.encode('ascii') + b"\n")
 tn.read_until(EXEC_PROMPT)
 
-# Do no display '-- more --'
+# '-- more --' を削除
 tn.write(NOPAGER + b"\n")
 tn.read_until(EXEC_PROMPT)
 
-# If this is deleted, "#" is not displayed at the beginning of the immediately
-# following command. Is there any other good way?
+# 行頭に待ちプロンプトを表示させるための改行
 tn.write(b"\n")
 
-# Run commands
+# コマンド入力
 for command in commands:
     tn.write(command.encode('ascii') + b"\n")
 tn.write(EXIT + b"\n")
 
-# Display result
+# 結果表示
 print(tn.read_all().decode('ascii'))
 
